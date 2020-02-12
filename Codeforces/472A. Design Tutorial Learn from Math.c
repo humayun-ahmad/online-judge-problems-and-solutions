3183dd64d0f4caff57159997c7979cfc4e53929a
Humayun Ahmad Rajib <humayun.ahmad.rajib@gmail.com>
2020-02-12 21:53:42 +0600

    472A Design Tutorial Learn from Math

---
+++ Codeforces/472A. Design Tutorial Learn from Math.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+    int n,i,a,b,str[]={2,3,5,7};
+
+    scanf("%d", &n);
+
+    if(n%2==0){
+        n = n / 2;
+        a = n - 1;
+        b = n + 1;
+    }
+    else{
+        n = n / 2;
+        a = n - 1;
+        b = n + 2;
+    }
+
+    while(){
+       if()
+    }
+
+    return 0;
+}
