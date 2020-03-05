1e88ab09647389a9d98f5b00040f3c79c493f439
Humayun Ahmad Rajib <humayun.ahmad.rajib@gmail.com>
2020-03-05 10:48:47 +0600

    A Helpfull math

---
+++ Codeforces/A. A Helpfull math.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int i,j,str1[300];
+    char str[300];
+
+    gets(str);
+    L = strlen(str);
+
+    for(i = 0,j=0;i < L;i+=2,j++){
+        str1[j] = str[i] - '0';
+    }
+    str1[] = sort()
+
+    return 0;
+}
