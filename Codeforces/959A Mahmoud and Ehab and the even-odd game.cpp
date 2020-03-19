20afcecde13b042bb90a78c24cd7e14ec5caa121
humayun-ahmad <humayunraj789@gmail.com>
2020-03-19 22:23:49 +0600

    959A Mahmoud and Ehab and the even-odd game

---
+++ Codeforces/959A Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+ 
+int main()
+{
+	int input_num;
+ 
+	scanf("%d",&input_num);
+ 
+	if(input_num%2==0){
+	printf("Mahmoud\n");
+	}
+ 
+	else{
+	printf("Ehab\n");
+	}
+ 
+ 
+	return 0;
+}
